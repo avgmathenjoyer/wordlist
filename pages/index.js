44ca940a938a3709f69cb3b594e48e273c7bdb1b
avgmathenjoyer <avgmathenjoyer@gmail.com>
2022-06-21 00:14:32 +0200

    add front screen

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,17 @@
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-6xl text-center">Wordlist</h1>
+    <div className="h-screen w-screen flex items-center justify-center font-display text-center">
+      <div className="m-4 w-11/12 h-5/6 shadow-2xl flex rounded flex-col md:flex-row items-center bg-slate-50">
+        <div className="h-3/6 mx-4">
+          <h1 className="text-6xl my-12">Turn a <span className="text-green-600">sentence</span> into a <span className="text-green-600">playlist</span></h1>
+          <h3 className="text-xl my-4">Type in a sentence and turn it into a Spotify playlist</h3>
+          <button className="bg-green-600 text-3xl rounded-xl my-5 text-white p-3">Login with Spotify to continue</button>
+        </div>
+        <div className="flex mx-auto invisible md:visible">
+          <img src="icon.svg" alt="svg"/>
+        </div>
+      </div>
     </div>
   )
 }
